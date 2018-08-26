ca0636a1318f71170282da7d04594f859f3e3322
Adriano Maciel <adriano_mail@hotmail.com>
2018-08-25 21:25:46 -0300

    Iniciadas modificações na página principal

--- Listadecompras/app/src/main/java/br/com/livrokotlin/listadecompras/MainActivity.kt
+++ Listadecompras/app/src/main/java/br/com/livrokotlin/listadecompras/MainActivity.kt
@@ -1,12 +1,12 @@
 package br.com.livrokotlin.listadecompras
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,25 +20,17 @@ class MainActivity : AppCompatActivity() {
                 android.R.layout.simple_expandable_list_item_1
         )
 
+        // ao clicar no botão de adicionar
+        btn_adicionar.setOnClickListener {
+            // cirando uma Intent explícita para abrir a tela de cadastro
+            val intent = Intent(this, CadastroActivity::class.java)
+
+            // iniciando a atividade
+            startActivity(intent)
+        }
+
         // ligando o adaptador no elemento de tela
         list_view_produtos.adapter = produtosAdapter
-
-        // ao clicar no botão de inserir
-        btn_inserir.setOnClickListener {
-            val produto = txt_produto.text.toString()
-
-            // inserir apenas se não estiver vazia
-            if( produto.isNotEmpty() ){
-                produtosAdapter.add(produto)
-
-                // limpar o campo ao inserir
-                txt_produto.text.clear()
-            }
-            // caso contrário, disparar um erro
-            else {
-                txt_produto.error = "Preencha um valor"
-            }
-        }
 
         // ao tocar e segurar em um item por alguns segundos
         list_view_produtos.setOnItemLongClickListener {
