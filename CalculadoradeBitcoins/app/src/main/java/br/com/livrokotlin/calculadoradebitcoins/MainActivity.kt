def4f6ea10a31011178e4b9b14f57c8d58a562b9
Adriano Maciel <adriano_mail@hotmail.com>
2018-09-09 17:26:59 -0300

    Tratando objeto json retornado pela api

--- CalculadoradeBitcoins/app/src/main/java/br/com/livrokotlin/calculadoradebitcoins/MainActivity.kt
+++ CalculadoradeBitcoins/app/src/main/java/br/com/livrokotlin/calculadoradebitcoins/MainActivity.kt
@@ -6,6 +6,7 @@ import org.jetbrains.anko.alert
 import java.net.URL
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,8 +27,11 @@ class MainActivity : AppCompatActivity() {
             // acessar api REST
             val resposta = URL(API_URL).readText()
 
+            // recuperando o valor da última cotação dentro do objeto json
+            cotacaoBitcoin = JSONObject(resposta).getJSONObject("ticker").getDouble("last")
+
             uiThread {
-                alert(resposta).show()
+                alert("$cotacaoBitcoin").show()
             }
         }
     }
