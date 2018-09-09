638e79e5dcf9d243cdb1bb0b452ca8b7c5f2e306
Adriano Maciel <adriano_mail@hotmail.com>
2018-09-09 17:23:09 -0300

    Adicionada chamada assíncrona para API

--- CalculadoradeBitcoins/app/src/main/java/br/com/livrokotlin/calculadoradebitcoins/MainActivity.kt
+++ CalculadoradeBitcoins/app/src/main/java/br/com/livrokotlin/calculadoradebitcoins/MainActivity.kt
@@ -2,11 +2,33 @@ package br.com.livrokotlin.calculadoradebitcoins
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.alert
+import java.net.URL
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
+
+    val API_URL = "https://www.mercadobitcoin.net/api/BTC/ticker/"
+    var cotacaoBitcoin: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buscarCotacao()
+    }
+
+    fun buscarCotacao(){
+        // acessando de forma assíncrona, evitando travamento do app nesta linha
+        // no caso do site demorar responder
+        doAsync{
+            // acessar api REST
+            val resposta = URL(API_URL).readText()
+
+            uiThread {
+                alert(resposta).show()
+            }
+        }
     }
 }
